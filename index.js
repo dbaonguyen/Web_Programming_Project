5e3a6b048ab1283596d0e3d023428e99bdea48cb
Nguyen Danh Bao <baond39@gmail.com>
2023-09-10 21:40:09 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('kid-fashions')
+    res.render('index')
 });
 
 app.listen(port, () => {
