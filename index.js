47a56eee21a924fed0d2e6aa508174fb31d26b4c
Nguyen Danh Bao <baond39@gmail.com>
2023-09-12 21:12:08 +0700

    Update

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
