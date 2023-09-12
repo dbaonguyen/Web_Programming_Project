7b2a2d5bffd424f2efca393a57e5ba992f250c9d
Anhtu <s3975032@rmit.edu.vn>
2023-09-12 22:17:21 +0700

    update controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -10,7 +10,25 @@ const productController = {
           res.status(500).json(err);
         }
       },    
-}
+
+    addProduct: async (req, res) =>{
+        try {
+          const newProduct = new Product(req.body);
+          const savedProduct = await newProduct.save();
+          res.status(200).json(savedProduct)
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
+    getAllProduct: async (req, res) =>{
+        try{
+          const products = await Product.find();
+          res.status(200).json(products);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    },
+};    
 
 
 module.exports = productController; 
--- routes/product.js
+++ routes/product.js
@@ -2,5 +2,7 @@ const ProductController = require('../controllers/ProductController');
 const router = require("express").Router();
 
 router.put("/:id", ProductController.updateProduct);
+router.post("/", ProductController.addProduct)
+router.get("/", ProductController.getAllProduct)
 
 module.exports = router;
