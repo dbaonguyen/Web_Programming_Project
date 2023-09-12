671131af2f433329b648cc9579804ffee37deeb0
hoangdv <duongviethoang2042002@gmail.com>
2023-09-12 21:52:07 +0700

    update

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,8 +1,16 @@
 const Product= require("../model/Product");
 
 const productController = {
-
+    updateAuthor: async (req, res) => {
+        try {
+          const product = await Product.findById(req.params.id);
+          await product.updateOne({ $set: req.body });
+          res.status(200).json("Updated successfully!");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },    
 }
 
 
-module.exports = productController;
+module.exports = productController; 
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,6 @@
 const ProductController = require('../controllers/ProductController');
 const router = require("express").Router();
 
+router.put("/:id", ProductController.updateProduct);
 
 module.exports = router;
