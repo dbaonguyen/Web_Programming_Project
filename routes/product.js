4c9c298c6a7f11496874d051164d7aae767f2a3d
Anhtu <s3975032@rmit.edu.vn>
2023-09-17 00:20:19 +0700

    update vendor auth

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const route = express.Router();
+const passport = require('passport');
 
 const services = require('../services/render');
 const controller = require('../controllers/ProductController')
 const checkAuthentication = require("../middleware/checkAuthentication");
 
-route.get("/vendor", services.vendor);
+route.get("/vendor",checkAuthentication.checkAuthenticated, services.vendor);
 
 route.get('/add-product', services.add_product);
 
 route.get('/update-product', services.update_product);
+
+route.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+      successRedirect: '/vendor', 
+      failureRedirect: '/login', 
+    })(req, res, next);
+  });
 
 //api
 route.post("/api/products", controller.create);
