02ab5ef1cd4dba04970673edd08e016feaf0957f
duck <s3978210@rmit.edu.vn>
2023-09-18 23:48:05 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -13,56 +13,19 @@ route.get('/add-product', services.add_product);
 
 route.get('/update-product', services.update_product);
 
-// route.get ("/api/products", async (req, res) => {
+// route.get('/api/products', async (req, res) => {
 //     try {
-//         const page = parseInt(req.query.page) - 1 || 0;
-// 		const limit = parseInt(req.query.limit) || 2;
-//         const search = req.query.search || "";
-//         let sort = req.query.sort || "Best Match";
-//         let size = req.query.size || "All";
-//         const sizeOptions = [
-//             "XL",
-// 			"L",
-// 			"XM",
-// 			"M",
-// 			"XS",
-// 			"S",
-//         ];
-//         size === "All"
-// 			? (size = [...sizeOptions])
-// 			: (size = req.query.size.split(","));
-//         req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
-//         let sortBy = {};
-// 		if (sort[1]) {
-// 			sortBy[sort[0]] = sort[1];
-// 		} else {
-// 			sortBy[sort[0]] = "Best Match";
-// 		}
-//         const products = await Product.find({ name: { $regex: search, $options: "i" } })
-// 			.where("size")
-// 			.in([...size])
-// 			.sort(sortBy)
-// 			.skip(page * limit)
-// 			.limit(limit);
-//         const total = await Product.countDocuments({
-// 			size: { $in: [...size] },
-// 			name: { $regex: search, $options: "i" },
-// 		});
-//         const response = {
-// 			error: false,
-// 			total,
-// 			page: page + 1,
-// 			limit,
-// 			size: sizeOptions,
-// 			products,
-// 		};
-
-// 		res.status(200).json(response);
-//     } catch (err) {
-// 		console.log(err);
-// 		res.status(500).json({ error: true, message: "Internal Server Error" });
+//       const { size, price } = req.query;
+//       const filter = {};
+//       if (size) filter.size = size;
+//       if (price) filter.price = price;
+//       const products = await Product.find(filter);
+//       res.status(200).json(products);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: true, message: 'Internal Server Error' });
 //     }
-// });
+//   });
 
 //api
 route.post("/api/products", controller.create);
@@ -71,4 +34,3 @@ route.put("/api/products/:id", controller.update);
 route.delete("/api/products/:id", controller.delete);
 
 module.exports = route;
-  
