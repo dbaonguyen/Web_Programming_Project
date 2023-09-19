8c643664f1646d9dacab112a382098faca6a30af
duck <s3978210@rmit.edu.vn>
2023-09-20 00:11:35 +0700

    update

--- routes/category.js
+++ routes/category.js
@@ -6,14 +6,19 @@ router.get("/:categoryName", async (req, res) => {
   try {
     let name = req.isAuthenticated() ? req.user.username : undefined;
     const categoryName = req.params.categoryName;
-    console.log(categoryName);
-    // Query the database to find all products in the specified category
-    const products = await Product.find({
-      category: categoryName,
-    }).exec();
+    const size = req.query.size; // Get the size from the query parameter
 
-    // Render an EJS template with the products
-    res.render("category-products", { categoryName, products,name });
+    // Build the query object
+    const query = { category: categoryName };
+    if (size && size !== "all") {
+      query.size = size;
+    }
+
+    // Query the database to find products based on category and size
+    const products = await Product.find(query).exec();
+
+    // Render the EJS template with the products, selected size, and category name
+    res.render("category-products", { categoryName, products, name, selectedSize: size });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
